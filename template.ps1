f48bc00027cebf6653a72e90bb7058cc2c0fa30c
Chad Thomsen <chad.thomsen@gmail.com>
2020-06-03 07:31:10 -0400

    Created template

---
+++ template.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+	Changes the account a service runs under    
+.NOTES
+	 Created on:   	11/28/2014
+	 Created by:   	Chad Thomsen
+	 Filename:     	template.ps1
+.EXAMPLE
+    xxxx
+.PARAMETER PARAM1
+ 	xxxx
+.PARAMETER PARAM2
+	xxxx
+.PARAMETER PARAM3
+	xxxx
+.PARAMETER PARAM4
+	xxxx
+#>
+
+[CmdletBinding()]
+[OutputType()]  
+param (
+
+)
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
